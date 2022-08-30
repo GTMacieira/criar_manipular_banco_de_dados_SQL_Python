68bb94021476e1b737712848f14e9f855ccfe573
GTMacieira <macieira.gtalpo@gmail.com>
2022-08-29 22:40:36 -0300

    Criadas novas fuñcoes, logando ao banco php myadmin, criacao de tabela não funciona

--- banco_dados_Python_SQL.py
+++ banco_dados_Python_SQL.py
@@ -18,8 +18,9 @@ def create_server_connection(host_name, user_name, user_password):
     try:
         connection=mysql.connector.connect(
             host=host_name,
+            port=3306,
             user=user_name,
-            passwd=user_password
+            #passwd=user_password
         )
         print("MySQL Dtabase connection successful")
     #Texto caso não consiga conectar
@@ -31,12 +32,57 @@ def create_server_connection(host_name, user_name, user_password):
 #Função que recebe dois argumentos e executa uma consulta no servidor através da conexão
 def create_database(connection, query):
     cursor = connection.cursor()
+    #tentar criar banco de dados
     try:
         cursor.execute(query)
         print("Dtabase created succrssfully")
+    #Texto caso não consiga
     except Error as err:
         print(f'Error: {err}')
 
+#conectar diretamente em um banco de dados especifico
+def create_db_connection(host_name,user_name,User_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            port=3306,
+            user = user_name,
+            #passwd = user_password,
+            database = db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f'Error: {err}')
+    return connection
+
+#garante que os comandao detalhados na consulta SQK sajam implementados
+def execute_query(connection,query):
+    cursor=connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print('Query successful')
+    except Error as err:
+        print(f'Error: {err}')
+
+create_titles_table = """"
+CREATE TABLE titles(
+    title_id INT PRIMARY KEY,
+    title_name VARCHAR(40) NOT NULL,
+    jp_title VARCHAR(100) NOT NULL,
+    released_chaps INT NOT NULL,
+    read_chaps INT,
+    last_down DATE NOT NULL,
+    kindle VARCHAR(3),
+    status INT
+    );
+    """
 
 pw= 'Pyth0n&SQLP@ss'
-connection = create_server_connection("localhost", "root", pw)
+connection = create_db_connection('localhost', 'root', pw, 'lista_de_mangás')
+#create_database_query = "CREATE DATABASE lista_de_mangás"
+#create_database(connection,create_database_query)
+execute_query(connection,create_titles_table)
+
+
